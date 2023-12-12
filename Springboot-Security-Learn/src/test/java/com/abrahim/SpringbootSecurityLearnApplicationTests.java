19ff5adbdfafcad1fb7c9b9fe1f4446f9149a5dc
abrahimcse <abrahimcse@gmail.com>
2023-12-12 18:06:22 +0600

    Spring boot Security-First Project Initialized

---
+++ Springboot-Security-Learn/src/test/java/com/abrahim/SpringbootSecurityLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abrahim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurityLearnApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
